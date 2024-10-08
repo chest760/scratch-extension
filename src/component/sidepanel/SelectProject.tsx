edfe1f61e8b6ba5fff5affa0a331ce8ca22dddb3
小野結衣 <yui.yui.o.121220@gmail.com>
2024-10-08 15:11:23 +0900

    ボタンとクイズページ追加

--- src/component/sidepanel/SelectProject.tsx
+++ src/component/sidepanel/SelectProject.tsx
@@ -3,8 +3,10 @@ import City from "../../assets/City.png";
 import Farm from "../../assets/Farm.png";
 import Gym from "../../assets/Gym.png";
 import styles from "../../sidepanel/index.module.scss";
+import { useNavigate } from "react-router-dom"; 
 
 export const SelectProject = () => {
+const navigate = useNavigate();
   return (
     <>
       <h1 className="title">スクラッチ拡張</h1>
@@ -18,12 +20,20 @@ export const SelectProject = () => {
             cursor: "pointer",
           }}
         >
+
           <h2 style={{ margin: "10px 0px 5px 0px", textAlign: "center" }}>
             変数を学ぶ
           </h2>
           <div style={{ flexGrow: 1, margin: "0 auto" }}>
-            <img src={Farm} className={styles.farm} />
+            {/* 画像にonClickを追加 */}
+            <img 
+              src={Farm} 
+              className={styles.farm} 
+              onClick={() => navigate("/variablequiz")} // 画像をクリックしたらnavigateが実行される
+              style={{ cursor: "pointer" }} // ポインターを変えることでクリック可能と分かりやすくする
+            />
           </div>
+
         </div>
 
         <div
@@ -34,13 +44,21 @@ export const SelectProject = () => {
             cursor: "pointer",
           }}
         >
+       
           <h2 style={{ margin: "10px 0px 5px 0px", textAlign: "center" }}>
             並列処理を学ぶ
           </h2>
           <div style={{ flexGrow: 1, margin: "0 auto" }}>
-            <img src={City} className={styles.farm} />
+            {/* 画像にonClickを追加 */}
+            <img 
+              src={City} 
+              className={styles.farm} 
+              onClick={() => navigate("/parallelquiz")} // 画像をクリックしたらnavigateが実行される
+              style={{ cursor: "pointer" }} // ポインターを変えることでクリック可能と分かりやすくする
+            />
           </div>
         </div>
+
 
         <div
           style={{
@@ -50,14 +68,23 @@ export const SelectProject = () => {
             cursor: "pointer",
           }}
         >
+
           <h2 style={{ margin: "10px 0px 5px 0px", textAlign: "center" }}>
             繰り返しを学ぶ
           </h2>
           <div style={{ flexGrow: 1, margin: "0 auto" }}>
-            <img src={Gym} className={styles.farm} />
+            {/* 画像にonClickを追加 */}
+            <img 
+              src={Gym} 
+              className={styles.farm} 
+              onClick={() => navigate("/repetitionquiz")} // 画像をクリックしたらnavigateが実行される
+              style={{ cursor: "pointer" }} // ポインターを変えることでクリック可能と分かりやすくする
+            />
           </div>
         </div>
       </Container>
     </>
   );
 };
+
+
