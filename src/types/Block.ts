e5ed987da9671a1351f73148b2c48155e0b7856c
chest760 <chestnut0725@gmail.com>
2024-10-08 14:07:29 +0900

    add category

--- src/types/Block.ts
+++ src/types/Block.ts
@@ -1,4 +1,12 @@
-export type BlockType = "MOVE RIGHT" | "MOVE LEFT" | "MOVE UP" | "MOVE DOWN" | "TURN RIGHT" | "TURN LEFT" | "HOP" | "GO HOME"
+export type MotionType = "MOVE RIGHT" | "MOVE LEFT" | "MOVE UP" | "MOVE DOWN" | "TURN RIGHT" | "TURN LEFT" | "HOP" | "GO HOME"
+export type StartType = "START ON GREEN FLAG" | "START ON TAP TIC" | "START ON BACK TIC" | "START ON ORANGE MESSAGE" | "SEND START ORANGE MESSAGE"
+export type LooksType = "SAY" | "GROW" | "SHRINK" | "RESET SIZE" | "HIDE" | "SHOW"
+export type SoundType = "PLAY POP"
+export type FlowType = "WAIT" | "STOP TIC" | "SET SPEED" | "REPEAT"
+export type StopType = "END" | "REPEAT FOREVER"
+
+
+export type BlockType = MotionType | StartType | LooksType | SoundType | FlowType | StopType
 
 export type Block = {
     x: number;
---
+++ src/types/Category.ts
@@ -0,0 +1,1 @@
+export type CategoryType = "Start" | "Motion" | "Looks" | "Sound" | "Flow" | "Stop" 
